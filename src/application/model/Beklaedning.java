b7df2aa828a448394df0c7120c9e895aa724376f
Jeppe <jeppe.wow@live.dk>
2022-03-24 12:32:27 +0100

    Færdigbehandlet Klasser under Øvrige

--- src/application/model/Beklaedning.java
+++ src/application/model/Beklaedning.java
@@ -1,4 +1,12 @@
 package application.model;
 
 public class Beklaedning {
+
+    private String størrelse;
+    private String mærke;
+
+    public Beklaedning (String størrelse, String mærke){
+        this.størrelse = størrelse;
+        this.mærke = mærke;
+    }
 }
--- src/application/model/Sammenpakning.java
+++ src/application/model/Sammenpakning.java
@@ -1,4 +1,10 @@
 package application.model;
 
 public class Sammenpakning {
+
+    private int antalOel;
+
+    public Sammenpakning (int antalOel){
+        this.antalOel = antalOel;
+    }
 }
