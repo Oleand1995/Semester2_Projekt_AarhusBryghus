0a3e8acba05f4639ae88749618d2b9c0da6c7673
madse333 <madsen170@yahoo.dk>
2022-03-24 12:34:09 +0100

    Anlæg, fustage og kulsyre er done

--- src/application/model/Anlæg.java
+++ src/application/model/Anlæg.java
@@ -1,7 +1,11 @@
 package application.model;
 
 public class Anlæg extends UdlejningsProdukter{
-    public Anlæg(String navn, int antal, double barPris, double butiksPris) {
+
+    private double leveringsPris;
+
+    public Anlæg(String navn, int antal, double barPris, double butiksPris, double leveringsPris) {
         super(navn, antal, barPris, butiksPris);
+        this.leveringsPris = leveringsPris;
     }
 }
