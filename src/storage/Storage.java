b9ff5185d991ba7be2fc0404ad9998c6d528a35b
Mads Nørskov <madsen170@yahoo.dk>
2022-03-25 09:24:15 +0100

    Lavet lidt på storage

---
+++ src/storage/Storage.java
@@ -0,0 +1,25 @@
+package storage;
+
+import application.model.Produkt;
+
+import java.util.ArrayList;
+
+public class Storage {
+
+    private static ArrayList<Salg> salg = new ArrayList<>();
+    private static ArrayList<Produkt> produkter = new ArrayList<>();
+    private static ArrayList<Udlejning> udlejninger = new ArrayList<>();
+
+    public static ArrayList<Salg> getSalg(){
+        return new ArrayList<Salg>(salg);
+    }
+
+    public static ArrayList<Produkt> getProdukter(){
+        return new ArrayList<Produkt>(produkter);
+    }
+
+    public static ArrayList<Udlejning> getUdlejninger(){
+        return new ArrayList<Udlejning>(udlejninger);
+    }
+
+}
