b4a6f7df01fd727ecbbc3a008f87faaa5785fac8
Mads Nørskov <madsen170@yahoo.dk>
2022-03-25 12:10:14 +0100

    Lavet en fadoel

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -1,6 +1,21 @@
 package application.controller;
 
+import application.model.Fadøl;
+import application.model.Produkt;
+import storage.Storage;
+
 public class Controller {
+
+    public static Produkt createFadoel(String navn, int antal, double barPris, double butiksPris, int klipPris, String størrelse){
+        Produkt produkt = new Fadøl(navn,antal,barPris,butiksPris,klipPris,størrelse);
+        Storage.addProdukt(produkt);
+        return produkt;
+    }
+
+    public static void initStorage(){
+        Controller.createFadoel("Forårsbryg", 1, 2, 3, 2, "Flaske");
+
+    }
 
 
 }
