eddeb6c53d05d925b8bc3fcdb43ad471a2a9a3e6
Mads Nørskov <madsen170@yahoo.dk>
2022-03-25 09:31:44 +0100

    Lavet Storage

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -8,20 +8,29 @@ import java.util.ArrayList;
 
 public class Storage {
 
-    private static ArrayList<Salg> salg = new ArrayList<>();
+    private static ArrayList<Salg> salgs = new ArrayList<>();
     private static ArrayList<Produkt> produkter = new ArrayList<>();
     private static ArrayList<Udlejning> udlejninger = new ArrayList<>();
 
+    //------------------------------------------------------------------------------------------------------------------
     public static ArrayList<Salg> getSalg(){
-        return new ArrayList<Salg>(salg);
+        return new ArrayList<Salg>(salgs);
     }
+    public static void addSalg(Salg salg){salgs.add(salg);}
+    public static void removeSalg(Salg salg){salgs.remove(salg);}
 
-    public static ArrayList<Produkt> getProdukter(){
-        return new ArrayList<Produkt>(produkter);
-    }
+    //------------------------------------------------------------------------------------------------------------------
+    public static ArrayList<Produkt> getProdukter(){return new ArrayList<Produkt>(produkter);}
+    public static void addProdukt(Produkt produkt){produkter.add(produkt);}
+    public static void removeProdukt(Produkt produkt){produkter.remove(produkt);}
 
+    //------------------------------------------------------------------------------------------------------------------
     public static ArrayList<Udlejning> getUdlejninger(){
         return new ArrayList<Udlejning>(udlejninger);
     }
+    public static void addUdlejning(Udlejning udlejning){udlejninger.add(udlejning);}
+    public static void removeUdlejning(Udlejning udlejning){udlejninger.remove(udlejning);}
+
+
 
 }
