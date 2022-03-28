5a81a0f6367e2271d196fd1799a4f8995bca81b8
magnu <magnus.s.larsen@hotmail.com>
2022-03-28 10:01:07 +0200

    Fixet procentrabat

--- src/application/model/ProcentRabat.java
+++ src/application/model/ProcentRabat.java
@@ -2,9 +2,9 @@ package application.model;
 
 public class ProcentRabat implements RabatBeregning {
 
-    @Override
-    public double rabat(double pris) {
-        double fradrag = pris * 0.3;
-        return pris + ((fradrag >= 0.3) ? fradrag : 3);
-    }
+        @Override
+        public double rabat(double pris) {
+            double fradrag = pris * 0.3;
+            return pris + ((fradrag >= 0.3) ? fradrag : 3);
+        }
 }
