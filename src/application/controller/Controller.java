f0843184b5bf94af8da4cbb6f80036401a5067e8
madse333 <madsen170@yahoo.dk>
2022-03-30 11:58:52 +0200

    Udviklet på opretSalgVindue, ændret lidt i produkt, controller og prisliste

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -40,12 +40,6 @@ public class Controller {
 
     public static ArrayList<Prisliste> getPrislister(){return Storage.getPrislister();}
 
-    public static void setPrisLister(Prisliste prisliste){
-        for (Produkt p : Storage.getProdukter()){
-            p.setPrisliste(prisliste);
-        }
-    }
-
     public static void sletPrisliste(Prisliste prisliste){
         if (Storage.getPrislister().contains(prisliste)){
             Storage.removePrisliste(prisliste);
