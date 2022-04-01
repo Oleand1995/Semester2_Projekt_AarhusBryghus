4fb5dde194d1b948b6d8d6871ebc91c4d41f0709
Jeppe <jeppe.wow@live.dk>
2022-04-01 13:07:20 +0200

    Tilføjet controller produkter, grupper og butik / bar

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -5,6 +5,8 @@ import javafx.collections.ObservableList;
 import storage.Storage;
 
 import javax.naming.ldap.Control;
+import java.net.PortUnreachableException;
+import java.net.SocketImpl;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -98,20 +100,275 @@ public class Controller {
 
 
     public static void initStorage(){
+
+        // Produktgrupper:
+
+        ProduktGruppe flaske = Controller.createproduktGruppe("Flaske");
         ProduktGruppe fadoel = Controller.createproduktGruppe("Fadøl");
-        Produkt storOel = Controller.createProdukt("Stor fed øl",fadoel);
-        Produkt lilleOel = Controller.createProdukt("Lille bitte øl",fadoel);
-        Produkt mellemOel = Controller.createProdukt("Semi øl",fadoel);
+        ProduktGruppe spiritus = Controller.createproduktGruppe("Spiritus");
+        ProduktGruppe fustage = Controller.createproduktGruppe("Fustage");
+        ProduktGruppe kulsyre = Controller.createproduktGruppe("Kulsyre");
+        ProduktGruppe malt = Controller.createproduktGruppe("Malt");
+        ProduktGruppe beklaedning = Controller.createproduktGruppe("Beklædning");
+        ProduktGruppe anlaeg = Controller.createproduktGruppe("Anlæg");
+        ProduktGruppe glas = Controller.createproduktGruppe("Glas");
+        ProduktGruppe sampakning = Controller.createproduktGruppe("Sampakning");
+        ProduktGruppe sodavand = Controller.createproduktGruppe("Sodavand");
+        ProduktGruppe snacks = Controller.createproduktGruppe("Snacks");
+        ProduktGruppe rundvisning = Controller.createproduktGruppe("Rundvisning");
+
+        // Produkter under flaske:
+
+        Produkt klosterbryg = Controller.createProdukt("Klosterbryg", flaske);
+        Produkt sweetGB = Controller.createProdukt("Sweet Georgia Brown", flaske);
+        Produkt extraPilsner = Controller.createProdukt("Extra Pilsner", flaske);
+        Produkt celebration = Controller.createProdukt("Celebration", flaske);
+        Produkt blondie = Controller.createProdukt("Blondie", flaske);
+        Produkt foraarsbryg = Controller.createProdukt("Forårsbryg", flaske);
+        Produkt indianPA = Controller.createProdukt("Indian Pale Ale", flaske);
+        Produkt julebryg = Controller.createProdukt("Julebryg", flaske);
+        Produkt juletoenden = Controller.createProdukt("Juletønden", flaske);
+        Produkt oldSA = Controller.createProdukt("Old Strong Ale", flaske);
+        Produkt fregattenJylland = Controller.createProdukt("Fregatten Jylland", flaske);
+        Produkt imperialStout = Controller.createProdukt("Imperial Stout", flaske);
+        Produkt tribute = Controller.createProdukt("Tribute", flaske);
+        Produkt blackMonster = Controller.createProdukt("Black Monster", flaske);
+
+        // Produkter under fadøl:
+
+        Produkt monsterbryg = Controller.createProdukt("Monster Bryg, 40 cl", fadoel);
+        Produkt jazzClassic = Controller.createProdukt("Jazz Classic, 40 cl", fadoel);
+        Produkt extraPilsnerFadoel = Controller.createProdukt("Extra Pilsner, 40 cl", fadoel);
+        Produkt celebrationFadoel = Controller.createProdukt("Celebration, 40 cl", fadoel);
+        Produkt blondieFadoel = Controller.createProdukt("Blondie, 40 cl", fadoel);
+        Produkt foraarsbrygFadoel = Controller.createProdukt("Forårsbryg, 40 cl", fadoel);
+        Produkt indianPAFadoel = Controller.createProdukt("Indian Pale Ale, 40 cl", fadoel);
+        Produkt julebrygFadoel = Controller.createProdukt("Julebryg, 40 cl", fadoel);
+        Produkt imperialStoutFadoel = Controller.createProdukt("Imperial Stout, 40 cl", fadoel);
+        Produkt specialFadoel = Controller.createProdukt("Special, 40 cl", fadoel);
+
+        // Produkter under sodavand:
+
+        Produkt aeblebrus = Controller.createProdukt("Æblebrus", sodavand);
+        Produkt cola = Controller.createProdukt("Cola", sodavand);
+        Produkt nikoline = Controller.createProdukt("Nikoline", sodavand);
+        Produkt sevenUp = Controller.createProdukt("7-Up", sodavand);
+        Produkt vand = Controller.createProdukt("Vand", sodavand);
+
+        // Produkter under snacks:
+
+        Produkt chips = Controller.createProdukt("Chips", snacks);
+        Produkt peanuts = Controller.createProdukt("Peanuts", snacks);
+        Produkt oelpoelser = Controller.createProdukt("Ølpølser", snacks);
+
+        // Produkter under spiritus:
+
+        Produkt whisky = Controller.createProdukt("Whisky 45%, 50 cl rør", spiritus);
+        Produkt whiskyFireCL = Controller.createProdukt("Whisky, 4 cl", spiritus);
+        Produkt whiskyCL = Controller.createProdukt("Whisky 43%, 50 cl rør", spiritus);
+        Produkt egesplint = Controller.createProdukt("u/ egesplint", spiritus);
+        Produkt egesplintM = Controller.createProdukt("m/ egesplint", spiritus);
+        Produkt wGB = Controller.createProdukt("2 gange Whisky, Glas + Brikker", spiritus);
+        Produkt liqourOA = Controller.createProdukt("Liquor of Aarhus", spiritus);
+        Produkt lyngGin = Controller.createProdukt("Lyng Gin, 50 cl", spiritus);
+        Produkt lynGinFireCl = Controller.createProdukt("Lyng Gin, 4 cl", spiritus);
+
+        // Produkter under fustage:
+
+        Produkt klosterbrygFustage = Controller.createProdukt("Klosterbryg, 20 liter", fustage);
+        Produkt jazzClassicFustage = Controller.createProdukt("Jazz Classic, 25 liter", fustage);
+        Produkt extraPilsnerFustage = Controller.createProdukt("Extra Pilsner, 25 liter", fustage);
+        Produkt celebrationFustage = Controller.createProdukt("Celebration, 20 liter", fustage);
+        Produkt blondieFustage = Controller.createProdukt("Blondie, 25 liter", fustage);
+        Produkt foraarsbrygFustage = Controller.createProdukt("Forårsbryg, 20 liter", fustage);
+        Produkt indianPAFustage = Controller.createProdukt("Indian Pale Ale, 20 liter", fustage);
+        Produkt julebrygFustage = Controller.createProdukt("Julebryg, 20 liter", fustage);
+        Produkt imperialStountFustage = Controller.createProdukt("Imperial Stout, 20 liter", fustage);
+        Produkt pant = Controller.createProdukt("Pant", fustage);
+
+        // Produkter under Kulsyre
+
+        Produkt kulsyreSeksKg = Controller.createProdukt("Kulsyre, 6 kg", kulsyre);
+        Produkt kulsyrePant = Controller.createProdukt("Pant", kulsyre);
+        Produkt kulsyreFireKg = Controller.createProdukt("Kulsyre, 4 kg", kulsyre);
+        Produkt kulsyreTiKg = Controller.createProdukt("Kulsyre, 10 kg", kulsyre);
+
+        // Produkter under Malt:
+
+        Produkt maltSaek = Controller.createProdukt("Malt, 25 kg sæk", kulsyre);
+
+        // Produkter under Beklædning:
+
+        Produkt tshirt = Controller.createProdukt("T-Shirt", beklaedning);
+        Produkt polo = Controller.createProdukt("Polo", beklaedning);
+        Produkt cap = Controller.createProdukt("Cap", beklaedning);
+
+        // Produkter under Anlæg:
+
+        Produkt enHane = Controller.createProdukt("1-hane", anlaeg);
+        Produkt toHane = Controller.createProdukt("2-hane", anlaeg);
+        Produkt flereHaner = Controller.createProdukt("Bar med flere haner", anlaeg);
+        Produkt levering = Controller.createProdukt("Levering", anlaeg);
+        Produkt krus = Controller.createProdukt("Krus", anlaeg);
+
+        // Produkter under Glas:
+
+        Produkt uanset = Controller.createProdukt("Uanset Størrelse", glas);
+
+        // Produkter under sampakning:
+
+        Produkt gaveEt = Controller.createProdukt("Gaveæske, 2 øl, 2 glas", sampakning);
+        Produkt gaveTo = Controller.createProdukt("Gaveæske, 4 øl", sampakning);
+        Produkt traekasse = Controller.createProdukt("Trækasse, 6 øl", sampakning);
+        Produkt gavekurv = Controller.createProdukt("Gavekurv, 6 øl, 2 glas", sampakning);
+        Produkt traekasseTo = Controller.createProdukt("Trækasse, 6 øl, 6 glas", sampakning);
+        Produkt traekasseTre = Controller.createProdukt("Trækasse, 12 øl", sampakning);
+        Produkt papkasse = Controller.createProdukt("Papkasse, 12 øl", sampakning);
+
+        // Produkt under Rundvisning:
+
+        Produkt rundvisninger = Controller.createProdukt("Pr. Person dag", rundvisning);
+
+        // Prisliste i Bar:
 
         Prisliste bar = Controller.createPrisliste("Bar");
-        bar.createPris(20,lilleOel);
-        bar.createPris(40,mellemOel);
-        bar.createPris(60,storOel);
+
+        // Flaske i Bar:
+
+        // Fadøl i Bar:
+
+        // Sodavand i Bar:
+
+        bar.createPris(15, aeblebrus);
+        bar.createPris(15, cola);
+        bar.createPris(15, nikoline);
+        bar.createPris(15, sevenUp);
+        bar.createPris(10, vand);
+
+        // Snacks i Bar:
+
+        bar.createPris(10, chips);
+        bar.createPris(15, peanuts);
+        bar.createPris(30, oelpoelser);
+
+        // Spiritus i Bar:
+
+        bar.createPris(599, whisky);
+        bar.createPris(50, whiskyFireCL);
+        bar.createPris(499, whiskyCL);
+        bar.createPris(300, egesplint);
+        bar.createPris(350, egesplintM);
+        bar.createPris(80, wGB);
+        bar.createPris(175, liqourOA);
+        bar.createPris(350, lyngGin);
+        bar.createPris(40, lynGinFireCl);
+
+        // Kulsyre i Bar:
+
+        bar.createPris(400, kulsyreSeksKg);
+        bar.createPris(1000, kulsyrePant);
+
+        // Beklædning i Bar:
+
+        bar.createPris(70, tshirt);
+        bar.createPris(100, polo);
+        bar.createPris(30, cap);
+
+        // Sampakninger i Bar
+
+        bar.createPris(110, gaveEt);
+        bar.createPris(140,gaveTo);
+        bar.createPris(260, traekasse);
+        bar.createPris(260, gavekurv);
+        bar.createPris(350, traekasseTo);
+        bar.createPris(410, traekasseTre);
+        bar.createPris(370, papkasse);
+
+        // Prisliste i Butik:
 
         Prisliste butik = Controller.createPrisliste("Butik");
-        butik.createPris(55,storOel);
+
+        // Flaske i Butik:
+
+        butik.createPris(36, klosterbryg);
+        butik.createPris(36, sweetGB);
+        butik.createPris(36, extraPilsner);
+        butik.createPris(36, celebration);
+        butik.createPris(36, blondie);
+        butik.createPris(36, foraarsbryg);
+        butik.createPris(36, indianPA);
+        butik.createPris(36, julebryg);
+        butik.createPris(36, juletoenden);
+        butik.createPris(36, oldSA);
+        butik.createPris(36, fregattenJylland);
+        butik.createPris(36, imperialStout);
+        butik.createPris(36, tribute);
+        butik.createPris(60, blackMonster);
+
+        // Spiritus i Butik:
+
+        butik.createPris(599, whisky);
+        butik.createPris(499, whiskyCL);
+        butik.createPris(300, egesplint);
+        butik.createPris(350, egesplintM);
+        butik.createPris(80, wGB);
+        butik.createPris(175, liqourOA);
+        butik.createPris(350, lyngGin);
+
+        // Fustage i Butik:
+
+        butik.createPris(775, klosterbrygFustage);
+        butik.createPris(625, jazzClassicFustage);
+        butik.createPris(575, extraPilsnerFustage);
+        butik.createPris(775, celebrationFustage);
+        butik.createPris(700, blondieFustage);
+        butik.createPris(775, foraarsbrygFustage);
+        butik.createPris(775, indianPAFustage);
+        butik.createPris(775, julebrygFustage);
+        butik.createPris(775, imperialStountFustage);
+        butik.createPris(200, pant);
+
+        // Kulsyre i Butik:
+
+        butik.createPris(400, kulsyreSeksKg);
+        butik.createPris(1000, kulsyrePant);
+
+        // Malt i Butik:
+
+        butik.createPris(300, maltSaek);
+
+        // Beklædning i Butik:
+
+        butik.createPris(70, tshirt);
+        butik.createPris(100, polo);
+        butik.createPris(30, cap);
+
+        // Anlæg i Butik:
+
+        butik.createPris(250, enHane);
+        butik.createPris(400, toHane);
+        butik.createPris(500, flereHaner);
+        butik.createPris(500, levering);
+        butik.createPris(60, krus);
+
+        // Glas i Butik:
+
+        butik.createPris(15, uanset);
+
+        // Sampakning i Butik:
+
+        butik.createPris(110, gaveEt);
+        butik.createPris(140, gaveTo);
+        butik.createPris(260, traekasse);
+        butik.createPris(260, gavekurv);
+        butik.createPris(350, traekasseTo);
+        butik.createPris(410, traekasseTre);
+        butik.createPris(370, papkasse);
+
+        // Rundvisning i Butik:
+
+        butik.createPris(100, rundvisninger);
 
     }
-
 
 }
