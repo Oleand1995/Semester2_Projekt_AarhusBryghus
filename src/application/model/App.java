39cce79b7297f1ed38a01af3cff310bc52b59371
madse333 <madsen170@yahoo.dk>
2022-04-07 11:38:46 +0200

    Slettet virus og dertilhørende lort

--- src/application/model/App.java
+++ src/application/model/App.java
@@ -1,8 +1,15 @@
 package application.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class App {
 
     public static void main(String[] args) {
+
+
+//------------------------------------------------------------------------------------------------
+//                  **Elaboration - Iteration 1**
 
         //Opretter produkt gruppe
         ProduktGruppe fadØl_Gruppe = new ProduktGruppe("Fad øl gruppe");
@@ -21,9 +28,37 @@ public class App {
         Pris prisMedKlip = prisListe_Fredagsbar.createPrisOgKlip(50.0,gul_fadbamse,2);
 
 
+//------------------------------------------------------------------------------------------------
+//                  **Elaboration - Iteration 2**
+
+        //Opretter ordrelinjer.
+        OrdreLinje ordreLinje = new OrdreLinje(pris);
+        OrdreLinje ordreLinje1 = new OrdreLinje(prisMedKlip);
+
+        //Vores system fungere sådan at ordrelinjerne samles i guien,
+        //Og pushes efterfølgende ind i enten salg eller udlejning for
+        //at oprette enten salg eller udlejning.
+
+        //Jeg illustrere det her med en ny arrayliste
+        ArrayList<OrdreLinje> ordreLinjer = new ArrayList<>();
+        ordreLinjer.add(ordreLinje);
+        ordreLinjer.add(ordreLinje1);
+
+        //Salget oprettes med ordrelinjerne.
+        Salg salg = new Salg(LocalDateTime.now(),ordreLinjer,100,0);
+
+        //Udlejning oprettes med ordrelinjerne.
+        Udlejning udlejning = new Udlejning(LocalDateTime.now(),null,100,"Henrik",ordreLinjer);
 
 
+//------------------------------------------------------------------------------------------------
+//                  **Elaboration - Iteration 3**
 
+        //Opretter rabatBeregnere af de 2 typer.
+        RabatBeregning fastRabat = new FastRabat(50);
+        RabatBeregning procentRabat = new ProcentRabat(20);
+
+        //Vi tillader os at bruge ordrelinjer fra tidligere
 
     }
 
